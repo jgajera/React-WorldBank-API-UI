6b0b60cc9f39ccd96d99b2f8bf1d3e51b4f59be8
Janki G <jnkdesignhouse@gmail.com>
2021-05-01 16:30:13 -0400

    3 sections

--- src/Chart.js
+++ src/Chart.js
@@ -3,84 +3,67 @@ import React, { useState } from 'react';
 import './index.css';
 import { AreaChart, Area, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-import axios from 'axios';
-
 
 function Chart(props) {
-  const { data, error, country, countrySet } = props;
-  console.log(props.countrySet)
-
-  const [pickedCountry, errorCatch] = useState(false);
-  const [countryData] = useState([]);
-  const [indicator] = useState("");
-
-
-  if(props.countrySet!==undefined) {
-    axios.get('https://api.worldbank.org/v2/country/' + props.countrySet + '/indicator/DT.ODA.ALLD.CD?format=json')
-    .then(res => {
-        const countryData = res.data[1]; // [0] contains header/total information
-        pickedCountry(countryData[0].country.value);
-
-        // remove entries that have no value and set state
-        var removeEmptyYears = countryData.filter(product => product.value !== null);
-        countryData(removeEmptyYears);
-        
-        console.log(this.state.countryData);
-
-        // get indicator for chart title and set state
-        var indictor = this.state.countryData[0].indicator.value;
-        indicator(indicator);
-        }
-    )
-    .catch(error => errorCatch(true));
-}
-
+  const { xAxis, yAxis, detail, url, countryName, countryID, indicator, data, error} = props;
+  console.log(props.indicator.substring(0,3))
 
   return(
-    <div className="chart-container">
-        <div className={errorCatch}>
-            <p className="error-msg">No data for this country. Choose another country!</p>
-            <p className="explore-header">{props.country}</p>
+    <div className="section-container">
+        <div className={props.error}>
+            <div className={props.indicator.substring(0,3) + " grid"}>
+                <div className="text">
+                    <h2 className="">{props.indicator}</h2>
+                    <p>{props.detail}</p>
+                    <a className="indicator-btn" href={props.url}>More details &#10230;</a>
+                </div>
 
-            <div className="chart-draw" style={{ width: '80%', height: 300, margin: '0 auto' }}>
-                <ResponsiveContainer>
-                    <AreaChart width={730} height={250} data={data}
-                    margin={{ top: 10, right: 0, left: 50, bottom: 0 }}>
-                        <defs>
-                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#3fbbf3" stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor="#3fbbf3" stopOpacity={0.15}/>
-                            </linearGradient>
-                        </defs>
+                <div className="chart">
+                    <h2 className="chart-header">{props.countryName}</h2>
+                    
+                    <p className="error-msg">No data was taken in {props.countryName} for <span>{props.indicator}</span>.</p>
 
-                        <XAxis                     
-                            label={{ value: "Year", position: 'bottom' }}
-                            reversed dataKey="date" />
+                    <div className="chart-draw" style={{ width: '80%', height: 300, margin: '0 auto' }}>
+                        <ResponsiveContainer>
+                            <AreaChart width={730} height={250} data={data}
+                            margin={{ top: 10, right: 0, left: 20, bottom: 0 }}>
+                                <defs>
+                                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#3fbbf3" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#3fbbf3" stopOpacity={0.15}/>
+                                    </linearGradient>
+                                </defs>
 
-                        <YAxis
-                            label={{ value: "GDP ($)", position: 'top', offset:20 }}
-                            tickFormatter={(value) => new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                            maximumFractionDigits: 0,
-                            }).format(value)}/>
+                                <XAxis
+                                    label={{ value: "Year", position: 'bottom' }}
+                                    reversed dataKey="date" />
 
-                        <CartesianGrid strokeDasharray="3 3" />
+                                <YAxis
+                                    // label={{ value: , position: 'top', offset:20 }}
+                                    tickFormatter={(value) => new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                    maximumFractionDigits: 0,
+                                    }).format(value)}/>
 
-                        <Tooltip formatter={(value) => new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                            maximumFractionDigits: 0,
-                            }).format(value)}/>
+                                <CartesianGrid  
+                                    strokeDasharray="3 3" />
 
-                        <Area type="monotone" dataKey="value"       
-                            stroke="#022d5b" fillOpacity={1} fill="url(#colorUv)" />
-                    </AreaChart>
-                </ResponsiveContainer>
+                                <Tooltip formatter={(value) => new Intl.NumberFormat('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                    maximumFractionDigits: 0,
+                                    }).format(value)}/>
+
+                                <Area type="monotone"      
+                                    dataKey="value"       
+                                    stroke="#022d5b" fillOpacity={1} fill="url(#colorUv)" />
+                            </AreaChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
             </div>
-
         </div>
-
     </div>
   );
 }
--- src/DropDown.js
+++ src/DropDown.js
@@ -5,14 +5,23 @@ import axios from 'axios';
 import Chart from './Chart.js';
 // import { getDefaultNormalizer } from '@testing-library/dom';
 
+import Callout from './Callout.js';
+
 
 export default class DropDown extends React.Component {
     state = {
         countries: [],
+        pickedCountry: "",
         pickedCountryID: false, // set this to false to conditionally hide sections until user chooses country
-        countryData: [],
-        indicator: "",
-        errorCatch: false // set this to false to conditionally hide error message until API response comes back with success
+        countryData1: [],
+        indicator1: "",
+        errorCatch1: false, // set this to false to conditionally hide error message until API response comes back with success
+        countryData2: [],
+        indicator2: "",
+        errorCatch2: false, // set this to false to conditionally hide error message until API response comes back with success        
+        countryData3: [],
+        indicator3: "",
+        errorCatch3: false // set this to false to conditionally hide error message until API response comes back with success
     }
 
     componentDidMount() {
@@ -31,34 +40,89 @@ export default class DropDown extends React.Component {
 
     storeChoice(e) {
         this.setState({ pickedCountryID: e });
-        this.setState({ errorCatch : false});
-        // if(e!=="") {
-        //     axios.get('https://api.worldbank.org/v2/country/' + e + '/indicator/DT.ODA.ALLD.CD?format=json')
-        //     .then(res => {
-        //         const countryData = res.data[1]; // [0] contains header/total information
-        //         this.setState({ pickedCountryID: countryData[0].country.value });
 
-        //         // remove entries that have no value and set state
-        //         var removeEmptyYears = countryData.filter(product => product.value !== null);
-        //         this.setState({ countryData : removeEmptyYears});
+        this.setState({ errorCatch1 : false});
+        this.setState({ errorCatch2 : false});
+        this.setState({ errorCatch3 : false});
+
+        if(e!=="") {
+            axios.get('https://api.worldbank.org/v2/country/' + e + '/indicator/NY.GDP.MKTP.CD?format=json')
+            .then(res => {
+                const countryData = res.data[1]; // [0] contains header/total information
+                this.setState({ pickedCountryID: countryData[0].country.value });
+
+                // remove entries that have no value and set state
+                const removeEmptyYears = countryData.filter(product => product.value !== null);
+                this.setState({ countryData1 : removeEmptyYears});
                 
-        //         console.log(this.state.countryData);
+                // get country name for chart title and set state
+                const pickedCountry = this.state.countryData1[0].country.value;
+                this.setState({ pickedCountry : pickedCountry});
 
-        //         // get indicator for chart title and set state
-        //         var indictor = this.state.countryData[0].indicator.value;
-        //         this.setState({ indicator : indictor});
-        //         }
-        //     )
-        //     .catch(error => this.setState({ errorCatch : true}));
-        // }
+                // get indicator for chart title and set state
+                const indictor = this.state.countryData1[0].indicator.value;
+                this.setState({ indicator1 : indictor});
+                
+                }
+            )
+            .catch(error => this.setState({ errorCatch : true}));
+
+            axios.get('https://api.worldbank.org/v2/country/' + e + '/indicator/SM.POP.REFG?format=json')
+            .then(res => {
+                const countryData = res.data[1]; // [0] contains header/total information
+
+                // remove entries that have no value and set state
+                const removeEmptyYears = countryData.filter(product => product.value !== null);
+                this.setState({ countryData2 : removeEmptyYears});
+                
+                // get indicator for chart title and set state
+                const indictor = this.state.countryData2[0].indicator.value;
+                this.setState({ indicator2 : indictor});
+                
+                }
+            )
+            .catch(error => this.setState({ errorCatch2 : true}));
+
+            axios.get('https://api.worldbank.org/v2/country/' + e + '/indicator/SP.POP.TOTL?format=json')
+            .then(res => {
+                const countryData = res.data[1]; // [0] contains header/total information
+                console.log(countryData)
+
+                // remove entries that have no value and set state
+                const removeEmptyYears = countryData.filter(product => product.value !== null);
+
+                this.setState({ countryData3 : removeEmptyYears[0].value});
+                // get indicator for chart title and set state
+
+                const indictor = this.state.countryData3[0].indicator.value;
+                this.setState({ indicator3 : indictor});
+                
+                }
+            )
+            .catch(error => this.setState({ errorCatch3 : true}));
+        }
     }
 
     render() {     
-        var countryName = this.state.pickedCountry;
-        var indicator = this.state.indicator;
-        var countryData = this.state.countryData;
-        var countrySet = this.state.pickedCountryID;
-        var errorSet = this.state.errorCatch;
+        const countryName = this.state.pickedCountry;
+        const indicator = this.state.indicator1;
+        const countryData = this.state.countryData1;
+        const countrySet = this.state.pickedCountryID;
+        const errorSet = this.state.errorCatch1;
+        const detail1 = "GDP at purchaser's prices is the sum of gross value added by all resident producers in the economy plus any product taxes and minus any subsidies not included in the value of the products.";
+        const url1 = "https://datatopics.worldbank.org/world-development-indicators/themes/economy.html";
+
+
+        const indicator2 = this.state.indicator2;
+        const countryData2 = this.state.countryData2;
+        const errorSet2 = this.state.errorCatch2;
+        const detail2 = "Refugees are people who are recognized as refugees under the 1951 Convention Relating to the Status of Refugees or its 1967 Protocol, the 1969 Organization of African Unity Convention Governing the Specific Aspects of Refugee Problems in Africa, people recognized as refugees in accordance with the UNHCR statute, people granted refugee-like humanitarian status, and people provided temporary protection.";
+        const url2 = "https://datatopics.worldbank.org/world-development-indicators/themes/global-links.html";
+
+        const indicator3 = this.state.indicator3;
+        const countryData3 = this.state.countryData3;
+        const errorSet3 = this.state.errorCatch3;
+        const url3 = "https://datatopics.worldbank.org/world-development-indicators/themes/people.html";
 
         return (
             <div>
@@ -91,14 +155,47 @@ export default class DropDown extends React.Component {
                 <div className="chart-container">
                     { countrySet ?
                         <Chart
-                            country={countryName}
-                            countryID={countrySet}
-                            indicator = {indicator}
-                            data={countryData}
-                            error={ errorSet ? 
-                                "error-show" : "error-hide" }>
-                        </Chart>
-                    : null }
+                        chartType = "AreaChart"
+                        xAxis="GDP ($)"
+                        yAxis="Year"
+                        detail={detail1}
+                        url={url1}
+                        countryName={countryName}
+                        countryID={countrySet}
+                        indicator = {indicator}
+                        data={countryData}
+                        error={ errorSet ? 
+                            "error-show" : "error-hide" }>
+                    </Chart>
+                : null }
+
+                    { countrySet ?
+                        <Callout
+                        url={url3}
+                        countryName={countryName}
+                        indicator = {indicator3}
+                        data={countryData3}
+                        error={ errorSet3 ? 
+                            "error-show" : "error-hide" }>
+                    </Callout>
+                : null }
+
+
+                    { countrySet ?
+                        <Chart
+                        chartType = "BarChart"
+                        xAxis="Number of Refugees ($)"
+                        yAxis="Year"
+                        detail={detail2}
+                        url={url2}
+                        countryName={countryName}
+                        countryID={countrySet}
+                        indicator = {indicator2}
+                        data={countryData2}
+                        error={ errorSet2 ? 
+                            "error-show" : "error-hide" }>
+                    </Chart>
+                : null }
                 </div>
             </div>
         )
